d3aff0c9d667fca40c690ebb4182d42051bc9a22
Harshavardhana <harshavardhana4nov@gmail.com>
2026-09-10 09:18:30 -0400

    pushing Cyber lab1 files

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+echo "<h2>Login Attempt</h2>";
+echo "Username: " . $username . "<br>";
+echo "Password: " . $password . "<br>";
+?>
